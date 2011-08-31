fb991f34deeab90d24302b38cd16df3395047002
danx0r <danbmil99@gmail.com>
2011-08-30 18:22:43 -0700

    genshixate level.xml

---
+++ level.py
@@ -0,0 +1,17 @@
+import sys, os, json
+from genshi.template import MarkupTemplate
+DOCROOT = os.path.dirname(__file__)
+
+def application(environ, start_response):
+    template = "/template/level.xml"
+    f = open(DOCROOT + template)
+    tmpl = MarkupTemplate(f)
+    f.close()
+    stream = tmpl.generate()
+    output = stream.render('xhtml')
+
+    status = '200 OK'
+    response_headers = [('Content-type', 'text/html'),
+                        ('Content-Length', str(len(output)))]
+    start_response(status, response_headers)
+    return [output]
