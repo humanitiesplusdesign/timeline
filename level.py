0d598d58db8372affe665509cb4e4ca2fe567dcf
danx0r <danbmil99@gmail.com>
2011-08-30 19:24:02 -0700

    template py:for works

--- level.py
+++ level.py
@@ -2,12 +2,19 @@ import sys, os, json
 from genshi.template import MarkupTemplate
 DOCROOT = os.path.dirname(__file__)
 
+events = [
+    [0,0,0],
+    [4,0,0],
+    [0,4,0],
+    [4,4,4]
+    ]
+
 def application(environ, start_response):
     template = "/template/level.xml"
     f = open(DOCROOT + template)
     tmpl = MarkupTemplate(f)
     f.close()
-    stream = tmpl.generate(special_color="#ff0000")
+    stream = tmpl.generate(special_color="#ff0000", events=events)
     output = stream.render('xhtml')
 
     status = '200 OK'
