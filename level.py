1a1085f37465a662af0c3b30824b26ceb148215c
danx0r <danbmil99@gmail.com>
2011-08-31 00:10:03 -0700

    the basic mechanics of John Yorke

--- level.py
+++ level.py
@@ -9,18 +9,43 @@ import api
 from pprint import PrettyPrinter
 pprint = PrettyPrinter(indent=3, width=100)
 
-def application(environ, start_response):
-    events = [
-        [0,0,0,0],
-        [1,0,0,1],
-        [0,1,1,2],
-        ]
 
+def latlon2xy(lat, lon):
+    x = (lat-40)
+    y = (lon-12)
+    return x, y
+
+def ms2z(ms):
+    return (ms/1000000.0 + 6000.0) * .25
+
+def application(environ, start_response):
     colors = [
         "#ff0000",
         "#00ff00",
         "#0000ff",
         ]
+
+    events = []
+    evs = list(mong.Event.find({"Person":"John Yorke [GT][0001]"}))
+    for ev in evs:
+        lat = None
+        ms = None
+        if ev['Type'] == "Arrival":
+            if "MPlace" in ev:
+                place = mong.MPlace.find_one({"_id":ev['MPlace']})
+                if place and "Coords" in place:
+                    lat, lon = place['Coords'].split(",")
+                    lat = float(lat)
+                    lon = float(lon)
+            if "Date" in ev:
+                ms = ev['Date']['ms']
+
+        if lat and ms:
+            x, y = latlon2xy(lat, lon)
+            z = ms2z(ms)
+            print >> sys.stderr, "EVENT:", ev['MPlace'], x, y, z
+            events.append([x, y, z, 2])
+
     template = "/template/level.xml"
     f = open(DOCROOT + template)
     tmpl = MarkupTemplate(f)
